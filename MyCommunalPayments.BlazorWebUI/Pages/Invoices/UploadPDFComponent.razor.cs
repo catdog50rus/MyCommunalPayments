670a83e620dfb783f314e647d07f2b18bbf08e0f
catdog50rus <49200747+catdog50rus@users.noreply.github.com>
2020-12-15 16:38:01 +0300

    Добавление комментариев

--- MyCommunalPayments.BlazorWebUI/Pages/Invoices/UploadPDFComponent.razor.cs
+++ MyCommunalPayments.BlazorWebUI/Pages/Invoices/UploadPDFComponent.razor.cs
@@ -11,9 +11,8 @@ using System.Threading.Tasks;
 
 namespace MyCommunalPayments.BlazorWebUI.Pages.Invoices
 {
-    public class UploadPDFComponentBase : ComponentBase, IDisposable
+    public class UploadPDFComponentBase : ComponentBase
     {
-        private CancellationTokenSource cancelation;
         protected EditContext editContext;
         protected UploadViewModel uploadViewModel;
 
@@ -25,68 +24,91 @@ namespace MyCommunalPayments.BlazorWebUI.Pages.Invoices
 
         protected int orderId;
 
+        //Элементы управления интерфейсом
         protected bool isSubmit;
         protected bool isLoad;
         protected bool isDisable;
 
         protected override void OnInitialized()
         {
-            cancelation = new CancellationTokenSource();
             uploadViewModel = new UploadViewModel();
             editContext = new EditContext(uploadViewModel);
         }
 
+        /// <summary>
+        /// Обработка выбора файла в браузере
+        /// </summary>
+        /// <param name="eventArgs"></param>
         protected void OnChange(InputFileChangeEventArgs eventArgs)
         {
+            //Получаем файл из браузера
             uploadViewModel.File = eventArgs.File;
             editContext.NotifyFieldChanged(FieldIdentifier.Create(() => uploadViewModel.File));
-
+            //Если тип файла совпадает с *.pdf показываем кнопку подтвердить
             isSubmit = Path.GetExtension(uploadViewModel.File.Name).Equals(".pdf");
-            isDisable = false;
         }
 
+        /// <summary>
+        /// Подтверждаем выбор
+        /// </summary>
         protected void OnSubmit()
         {
+            //Меняем интерфейс
             isSubmit = false;
             isLoad = true;
             isDisable = false;
         }
 
+        /// <summary>
+        /// Показать / Скрыть уведомление 
+        /// </summary>
         protected void Disabled()
         {
             isDisable = !isDisable;
         }
 
+        /// <summary>
+        /// Запись файла в БД
+        /// </summary>
+        /// <returns></returns>
         protected async Task SaveFileAsync()
         {
             isLoad = false;
+            //Создаем поток
             using var stream = uploadViewModel.File.OpenReadStream();
 
             var ms = new MemoryStream();
+            //Присваиваем имя файла для хранения в БД
             Guid guid = Guid.NewGuid();
             var filename = $"{guid}.pdf";
-
+            //Считываем файл в память
             await stream.CopyToAsync(ms);
+            //Создаем и инициализируем экземпляр модели
             var order = new Order()
             {
                 OrderScreen = ms.ToArray(),
                 FileName = filename
             };
-
+            //Сохраняем модель в БД
             await FileUploadRepository.UploadAsync(order);
+            //Получаем id
             orderId = FileUploadRepository.OrderId;
             isLoad = false;
         }
 
-        public void Dispose() => cancelation.Cancel();
-
+        /// <summary>
+        /// Модель представления с кастомной валидацией
+        /// </summary>
         public class UploadViewModel
         {
-            [Required(ErrorMessage = "Необходимо выбрать файл со скааном платежки")]
+            [Required(ErrorMessage = "Необходимо выбрать файл со сканом платежки")]
             [FileValidation(new[] { ".pdf" })]
             public IBrowserFile File { get; set; }
         }
 
+        /// <summary>
+        /// Реализация валидации модели на тип файла
+        /// </summary>
         private class FileValidationAttribute : ValidationAttribute
         {
             public FileValidationAttribute(string[] allowedExtensions)
