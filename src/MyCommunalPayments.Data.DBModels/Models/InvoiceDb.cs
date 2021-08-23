a154488ffe162724c526ff4e3a8bc1702c3bc5de
Aleksei Perepelkin <catdog50rus@gmail.com>
2021-08-23 10:32:33 +0300

    #11 Реаструктуризация проекта Создание моделей БД Создание базовых оделей БД

---
+++ src/MyCommunalPayments.Data.DBModels/Models/InvoiceDb.cs
@@ -0,0 +1,19 @@
+﻿using MyCommunalPayments.Data.DBModels.Models.Base;
+
+namespace MyCommunalPayments.Data.DBModels.Models
+{
+    public class InvoiceDb : BaseDbModel
+    {
+        public int IdPeriod { get; set; }
+
+        public PeriodDb Period { get; set; }
+
+        public int IdProvider { get; set; }
+
+        public ProviderDb Provider { get; set; }
+
+        public decimal InvoiceSum { get; set; }
+
+        public bool Pay { get; set; }
+    }
+}
