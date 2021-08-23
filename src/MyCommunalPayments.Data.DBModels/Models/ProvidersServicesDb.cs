df41f7a14566b6af6a86beaaf833c56b5a8d46f7
Aleksei Perepelkin <catdog50rus@gmail.com>
2021-08-23 10:32:33 +0300

    #11 Реаструктуризация проекта Создание моделей БД Создание базовых оделей БД

---
+++ src/MyCommunalPayments.Data.DBModels/Models/ProvidersServicesDb.cs
@@ -0,0 +1,12 @@
+﻿using MyCommunalPayments.Data.DBModels.Models.Base;
+
+namespace MyCommunalPayments.Data.DBModels.Models
+{
+    public class ProvidersServicesDb : BaseDbModel
+    {
+        public int IdProvider { get; set; }
+        public ProviderDb Provider { get; set; }
+        public int IdService { get; set; }
+        public ServiceDb Service { get; set; }
+    }
+}
---
+++ src/MyCommunalPayments.Data.DBModels/Models/ServiceDb.cs
@@ -0,0 +1,10 @@
+﻿using MyCommunalPayments.Data.DBModels.Models.Base;
+
+namespace MyCommunalPayments.Data.DBModels.Models
+{
+    public class ServiceDb : BaseDbModel
+    {
+        public string NameService { get; set; }
+        public bool IsCounter { get; set; }
+    }
+}
